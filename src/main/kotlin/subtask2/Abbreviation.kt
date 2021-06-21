15285db78de347f61e2c6635996e58e7029a4e36
Karina Osoprilko <71908284+chamomilek@users.noreply.github.com>
2021-06-21 15:44:56 +0300

    feat: add solution

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,33 @@
 package subtask2
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+import subtask1.DateFormatter
+
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var temp = a.toUpperCase()
+        var i = 0
+        while (i < temp.length) {
+            var cur = false
+
+            for (ib in b.indices) {
+                if (temp[i] == b[ib]) {
+                    cur = true
+                }
+            }
+            if (!cur) {
+                temp = temp.removeRange(i, i + 1)
+            } else
+                i++
+        }
+
+        if (temp == b) return "YES" else return "NO"
+
     }
+}
+
+fun main() {
+    val v = Abbreviation()
+    println(v.abbreviationFromA("mnbTy", "BT"))
 }
